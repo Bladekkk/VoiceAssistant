928dd93b99090f2f97b0f8905f23bdff209911ac
stupid dev <kuraedov323@gmail.com>
2023-04-17 21:47:59 +0300

    Добавлена функция новостей

--- methods.py
+++ methods.py
@@ -17,7 +17,9 @@ import screen_brightness_control as sbc
 from text_to_num import stn
 import psutil
 from logs import Logger
-
+from rss_parser import Parser
+from requests import get
+import random
 
 loger = Logger()
 
@@ -27,9 +29,11 @@ def benchmark(func):
 		start = time.time()
 		return_value = func(*args, **kwargs)
 		end = time.time()
-		print('[*] Время выполнения: {} секунд.'.format(end-start))
+		print('[*] Время выполнения: {} секунд.'.format(end - start))
 		return return_value
+
 	return wrapper
+
 
 months = {
 	"1": "января",
@@ -56,6 +60,7 @@ weekdays = {
 	"6": "Воскресенье",
 }
 
+
 # текст выдаваемый vosk выдается как словарь, но в виде строки, поэтому я просто очищаю ее пока не останется лишь сам текст
 def clean(text):
 	text = text.replace('\n', '')
@@ -73,7 +78,8 @@ class Definer:
 		if os.path.exists(r'data\cmd.json'):
 			pass
 		else:
-			loger.fatal_err('Dataset hasn\'t be found') # Если не нашел датасет, просто прога превращается в кирпич и ничего не делает (я не смог отсюда закрыть окно)
+			loger.fatal_err(
+				'Dataset hasn\'t be found')  # Если не нашел датасет, просто прога превращается в кирпич и ничего не делает (я не смог отсюда закрыть окно)
 			raise SystemExit()
 
 		with open(r'data\cmd.json', 'r', encoding='utf-8') as file:
@@ -85,7 +91,7 @@ class Definer:
 		for command in self.cmd_list:
 			for txt in self.cmd_list[command]:
 				if txt in text:
-					loger.log(f'Recognized command: {command}') # Знаю, самое лучшее логирование
+					loger.log(f'Recognized command: {command}')  # Знаю, самое лучшее логирование
 					if output != '':
 						output = f'{output}\n{self.start_command(command, text)}'
 					else:
@@ -99,21 +105,21 @@ class Definer:
 	# Если в названии команды есть постфикс "_arg" то она принимает аргумент в виде распознанного текста, чтобы
 	# его обрабатывать на предмет нужных данных
 	def start_command(self, cmd, arg):
-		if str(cmd).endswith('_arg') is False: # Проверка на постфикс
-			out = eval(f'Functions().{cmd}()') # Запуск команды без аргумента
+		if str(cmd).endswith('_arg') is False:  # Проверка на постфикс
+			out = eval(f'Functions().{cmd}()')  # Запуск команды без аргумента
 		else:
-			out = eval(f'Functions().{cmd}("{arg}")') # Запуск команды с аргументом
-		return out # Возврат вывода вызванной функции
-			
+			out = eval(f'Functions().{cmd}("{arg}")')  # Запуск команды с аргументом
+		return out  # Возврат вывода вызванной функции
+
 
 # Класс с основными функциями которые выполняются по команде пользователя
 class Functions:
 	def __init__(self):
-		API = 'd8103ddb4c9d11d7f82ffad9cce5ee36' # Апи к сервису от погоды
-		conf = get_default_config() # Получение дефолтного конфига
-		conf["language"] = 'ru' # Изменить в конфиге язык на русский
+		API = 'd8103ddb4c9d11d7f82ffad9cce5ee36'  # Апи к сервису от погоды
+		conf = get_default_config()  # Получение дефолтного конфига
+		conf["language"] = 'ru'  # Изменить в конфиге язык на русский
 		try:
-			self.owm = OWM(API, conf) # Применить новый конфиг и подключить апи
+			self.owm = OWM(API, conf)  # Применить новый конфиг и подключить апи
 		except:
 			loger.warning('Отстутствует подключение к интернету')
 			self.owm = None
@@ -127,17 +133,17 @@ class Functions:
 		return f'Сегодня {a.day} {months[str(a.month)]}, {weekdays[str(a.weekday()).lower()]}'
 
 	def weather(self):
-		place = 'Северодвинск' # Задание места, по дефолту поставил Севск, но в будущем будет зависеть от настроек пользователя
+		place = 'Северодвинск'  # Задание места, по дефолту поставил Севск, но в будущем будет зависеть от настроек пользователя
 		try:
-			monitoring = self.owm.weather_manager().weather_at_place(place) # Вырвано из доков
-			weather = monitoring.weather # Тоже вырвано из доков
-			temp = weather.temperature('celsius') # Получение температуры по цельсия
-			status = weather.detailed_status # Краткий статус погоды, типо "Облачно"
+			monitoring = self.owm.weather_manager().weather_at_place(place)  # Вырвано из доков
+			weather = monitoring.weather  # Тоже вырвано из доков
+			temp = weather.temperature('celsius')  # Получение температуры по цельсия
+			status = weather.detailed_status  # Краткий статус погоды, типо "Облачно"
 			return (f'Сейчас {int(temp["temp"])}°C, {status}')
 		except:
 			return f"Неверно указано место ({place}) и/или отсутствует подключение к интернету"
 
-	def brightness_arg(self, text): # Смена яркости
+	def brightness_arg(self, text):  # Смена яркости
 		text = f' {text} '
 		for i in range(1, 101):
 			if f' {i} ' in text:
@@ -159,6 +165,21 @@ class Functions:
 	def volume_arg(self, text):
 		pass # Еще делаю
 
+	def news(self):
+
+		titles = []
+
+		rss_url = "https://habr.com/ru/rss/news/?fl=ru"
+		xml = get(rss_url)
+
+		parser = Parser(xml=xml.content, limit=10)
+		feed = parser.parse()
+
+		for new in reversed(feed.feed):
+			titles.append(new)
+		item = random.choice(titles)
+		return f'Заговолок: {item.title}\nСодержимое: {item.description.replace("Читать дальше →", "").replace("Читать далее", "")}'
+
 # Класс с функцией синтеза речи
 class TextToSpeech:
 	def __init__(self):
@@ -176,7 +197,7 @@ class TextToSpeech:
 	@benchmark
 	def say(self, audio):
 		audio = self.model.apply_tts(text=audio,
-									 speaker='kseniya') # Синтез речи
+									 speaker='kseniya')  # Синтез речи
 		# Воспроизведение синтеза
 		sd.play(audio, 48000)
 		time.sleep(len(audio) / 48000)
@@ -234,7 +255,7 @@ class RecordThreadHandler(QtCore.QThread):
 								#Thread(target=lambda: self.tts.say(audio=output)).start()
 								#self.tts.say(output)
 								self.signal.emit(['bot_responce', output])
-							
+
 							continue
 						else:
 							continue
@@ -242,6 +263,7 @@ class RecordThreadHandler(QtCore.QThread):
 					break
 			except Exception as e:
 				loger.warning(e)
+
 
 # Оставляю это место чтобы быстро проверять функции
 if __name__ == "__main__":
